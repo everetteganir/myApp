9e0d6ba9086789c3223b5eecf26613d5094d4719
bestbeans <ayu808@hawaii.edu>
2019-01-21 23:51:34 -1000

    Home Page before new sources were added

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { RegisterPage} from "../register/register";
 
 @Component({
   selector: 'page-home',
@@ -11,4 +12,9 @@ export class HomePage {
 
   }
 
+  signUp(){
+    this.navCtrl.push(RegisterPage);
+  }
+
 }
+
